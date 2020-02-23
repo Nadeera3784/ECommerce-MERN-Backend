517fbb9324ef6464b5f73d6b7f9962c16d6f2ae4
Mohammed-Abdelhady <mohammed_abdelhady97@hotmail.com>
2020-02-23 16:15:19 +0200

    conect mongodb

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
 const app = express()
 
 require('dotenv').config()
+
+//db connection
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB Connected'));
+ 
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
+});
 
 app.get('/', (req, res) => {
     res.send('start')
