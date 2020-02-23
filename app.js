e523e942f87099adb6916bb5f2cbda92ef982e33
Mohammed-Abdelhady <mohammed_abdelhady97@hotmail.com>
2020-02-23 16:38:15 +0200

    add routes controllers

--- app.js
+++ app.js
@@ -6,23 +6,25 @@ require('dotenv').config()
 
 //db connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => console.log('DB Connected'));
- 
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('DB Connected'));
+
 mongoose.connection.on('error', err => {
-  console.log(`DB connection error: ${err.message}`)
+    console.log(`DB connection error: ${err.message}`)
 });
 
-app.get('/', (req, res) => {
-    res.send('start')
-});
+// load routes
+const userRouter = require('./routes/user.route')
+app.use('/api', userRouter)
 
+app.use( (req, res, next) => {
+    res.send('page not founded');
+});
 const port = process.env.PORT || 8000
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
-
