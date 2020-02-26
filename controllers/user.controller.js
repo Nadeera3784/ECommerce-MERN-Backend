1289c01748956cf1ec51e47f624c39ea448ea623
Mohammed-Abdelhady <mohammed_abdelhady97@hotmail.com>
2020-02-26 21:31:26 +0200

    check if admin

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -22,3 +22,13 @@ exports.isAuht = (req, res, next) => {
     }
     next()
 }
+
+exports.isAdmin = (req, res, next) => {
+    if (req.profile.role === 0) {
+        return res.status(403).json({
+            error: "Admin resourse Access denied"
+        })
+    }
+
+    next()
+}
